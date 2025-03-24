4f2b823e3ab26de3c4914dccf318de91d072997e
AgonieZ <95156789+AgonieZ@users.noreply.github.com>
2025-03-24 12:51:24 +0800

    Update MoreContent.vue

--- src/components/MoreContent.vue
+++ src/components/MoreContent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="more-content">您可在此编写任意内容</div>
+  <div class="more-content">破败，腐烂，痛苦<br>墨绿的青苔将旧日的伤痕掩盖<br>美好，新生，欢乐<br>破茧的蝴蝶将希望的种子播撒</div>
 </template>
 
 <style lang="scss" scoped>
